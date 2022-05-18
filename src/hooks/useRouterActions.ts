48b8795106e3c4d7664c216accae8b9e60883b94
Alex Z <alex.z-9@ya.ru>
2022-05-18 20:53:29 +0300

    fix

--- src/hooks/useRouterActions.ts
+++ src/hooks/useRouterActions.ts
@@ -2,7 +2,7 @@ import { useDispatch } from "react-redux"
 import { bindActionCreators } from "redux";
 import actions from "../store/actions";
 
-export const useRouterActions = ()=>{
+export const useRouterActions = () => {
   const dispatch = useDispatch();
   return bindActionCreators(actions, dispatch);
 }
