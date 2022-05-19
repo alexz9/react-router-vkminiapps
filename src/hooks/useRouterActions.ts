8a45ccc26ccadced6d1b570842ce3a88ba6f29f3
Alex Z <alex.z-9@ya.ru>
2022-05-19 12:24:26 +0300

    test hooks

--- src/hooks/useRouterActions.ts
+++ src/hooks/useRouterActions.ts
@@ -1,8 +1,8 @@
-import { useDispatch } from "react-redux"
 import { bindActionCreators } from "redux";
+import store from "../store";
 import actions from "../store/actions";
 
 export const useRouterActions = () => {
-  const dispatch = useDispatch();
+  const dispatch = store.dispatch;
   return bindActionCreators(actions, dispatch);
 }
--- src/hooks/useRouterSelector.ts
+++ src/hooks/useRouterSelector.ts
@@ -1,4 +1,7 @@
-import { TypedUseSelectorHook, useSelector } from "react-redux";
-import { RootState } from "../store/reducers";
+import { useMemo } from "react";
+import store from "../store";
 
-export const useRouterSelector: TypedUseSelectorHook<RootState> = useSelector;
+export const useRouterSelector = () => {
+  const state = useMemo(() => store.getState(), [store.getState()]);
+  return state;
+}
