220a95aa218ffdc3f11ccfff73db634e15bf1063
Alex Z <alex.z-9@ya.ru>
2022-05-19 09:55:07 +0300

    add export type structure

--- src/index.tsx
+++ src/index.tsx
@@ -2,3 +2,4 @@ export { default as withRouter } from "./hoc/withRouter";
 export { default } from "./components/App";
 export { useRouterSelector } from "./hooks/useRouterSelector";
 export { useRouterActions } from "./hooks/useRouterActions";
+export type { IStructure } from "./types/app";
