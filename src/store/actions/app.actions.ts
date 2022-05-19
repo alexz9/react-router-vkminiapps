eb302ccb47c4320eca3f5197fdd350e5a792cd70
Alex Z <alex.z-9@ya.ru>
2022-05-19 13:00:04 +0300

    fix

--- src/store/actions/app.actions.ts
+++ src/store/actions/app.actions.ts
@@ -1,54 +1,54 @@
-import { EActionTypes, IrouterInit, ItoHash, ItoModal, ItoPanel, ItoPopout, ItoView, TActions } from "../../types/store";
+import { EActionTypes, IrouterInit, ItoHash, ItoModal, ItoPanel, ItoPopout, ItoView, IActions } from "../../types/store";
 
-export const toPopout = function (payload: ItoPopout["payload"]): TActions {
+export const toPopout = function (payload: ItoPopout["payload"]): IActions {
   return {
     type: EActionTypes.ROUTER_TO_POPOUT,
     payload
   }
 }
 
-export const toView = function (payload: ItoView["payload"]): TActions {
+export const toView = function (payload: ItoView["payload"]): IActions {
   return {
     type: EActionTypes.ROUTER_TO_VIEW,
     payload
   }
 }
 
-export const toPanel = function (payload: ItoPanel["payload"]): TActions {
+export const toPanel = function (payload: ItoPanel["payload"]): IActions {
   return {
     type: EActionTypes.ROUTER_TO_PANEL,
     payload
   }
 }
 
-export const toModal = function (payload: ItoModal["payload"]): TActions {
+export const toModal = function (payload: ItoModal["payload"]): IActions {
   return {
     type: EActionTypes.ROUTER_TO_MODAL,
     payload
   }
 }
 
-export const toBack = function (): TActions {
+export const toBack = function (): IActions {
   return {
     type: EActionTypes.ROUTER_TO_BACK
   }
 }
 
-export const toHash = function (payload: ItoHash["payload"]): TActions {
+export const toHash = function (payload: ItoHash["payload"]): IActions {
   return {
     type: EActionTypes.ROUTER_TO_HASH,
     payload
   }
 }
 
-export const routerInit = function (payload: IrouterInit["payload"]): TActions {
+export const routerInit = function (payload: IrouterInit["payload"]): IActions {
   return {
     type: EActionTypes.ROUTER_INIT,
     payload
   }
 };
 
-export const resetHistory = function (): TActions {
+export const resetHistory = function (): IActions {
   return {
     type: EActionTypes.ROUTER_RESET_HISTORY   
   }
