84544c0a2e244fe28c3842972673ea72b38bbb1e
Alex Z <alex.z-9@ya.ru>
2022-05-19 09:27:11 +0300

    fix

--- dist/store/actions.d.ts
+++
@@ -1,9 +0,0 @@
-import { IrouterInit, ItoHash, ItoModal, ItoPanel, ItoPopout, ItoView, TActions } from '../types/store';
-export declare const toPopout: (payload: ItoPopout['payload']) => TActions;
-export declare const toView: (payload: ItoView['payload']) => TActions;
-export declare const toPanel: (payload: ItoPanel['payload']) => TActions;
-export declare const toModal: (payload: ItoModal['payload']) => TActions;
-export declare const toBack: () => TActions;
-export declare const toHash: (payload: ItoHash['payload']) => TActions;
-export declare const routerInit: (payload: IrouterInit['payload']) => TActions;
-export declare const resetHistory: () => TActions;
--- dist/store/reducer.d.ts
+++
@@ -1,2 +0,0 @@
-import { IAppState, TActions } from "../types/store";
-export default function app(state: IAppState | undefined, action: TActions): IAppState;
